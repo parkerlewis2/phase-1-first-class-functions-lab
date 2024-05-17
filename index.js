fabbd85a8fae38e4e48ddb745c70af94b1d9d5b7
parker <parkerlewis2021@gmail.com>
2024-05-17 11:29:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const driverNames = [['Antonia', 'Nuru', 'Amari', 'Mo']]
+
+const returnFirstTwoDrivers = function(driverNames){
+    return driverNames.slice(0, 2)
+}
+const returnLastTwoDrivers = function (driverNames){
+    return driverNames.slice(-2)
+}
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+function createFareMultiplier(multiple) {
+    return function(outcome) {
+      return outcome * multiple
+    }
+  }
+
+function fareDoubler(fares){
+    return (fares + fares)
+}
+
+function fareTripler(moreFares){
+    return (moreFares + moreFares + moreFares)
+}
+
+const selectDifferentDrivers = function(driverNames, functionType){
+    return functionType(driverNames)
+}
